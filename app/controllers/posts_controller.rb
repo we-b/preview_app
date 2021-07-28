8a24dd7566de40c0c5070782f0ba7c6620b8b35b
Naoki-014 <70kio18.rf@gmail.com>
2021-07-29 08:11:48 +0900

    contentからtextに修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:content, :image)
+    params.require(:post).permit(:text, :image)
   end
 
   def set_post
--- db/migrate/20210720093015_create_posts.rb
+++ db/migrate/20210720093015_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :content, null: false
+      t.string :text, null: false
       t.timestamps
     end
   end
